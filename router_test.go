b01f89fcdaee526fac557222c67ea1a40a6734a7
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2016-12-11 11:49:02 +0100

    Added Test case for router - Multi route per router test case

--- router_test.go
+++ router_test.go
@@ -9,12 +9,11 @@ import (
 	"testing"
 )
 
-type routeTest struct {
+type routeTestCase struct {
 	title      string
 	path       string
 	method     string
 	statusCode int
-	kind       string
 	queries    map[string][]string
 	vars       map[string]string
 	route      func(r *Router, path string, method string, handler func(w http.ResponseWriter, r *http.Request))
@@ -22,7 +21,7 @@ type routeTest struct {
 
 func TestPath(t *testing.T) {
 
-	tests := []routeTest{
+	tests := []routeTestCase{
 		{
 			title:      "(GET) Path route with single path",
 			path:       "/api/",
@@ -35,8 +34,8 @@ func TestPath(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		t.Run(fmt.Sprintf("Test: %s path: %s method %s kind: %s", test.title, test.path, test.method, test.kind), func(t *testing.T) {
-			code, message, ok := testRoute(test)
+		t.Run(fmt.Sprintf("Test: %s path: %s method %s", test.title, test.path, test.method), func(t *testing.T) {
+			code, message, ok := testSingleRoute(test)
 
 			if !ok {
 				t.Errorf("Expected status code %v, Actucal status code %v, Actucal message %v", test.statusCode, code, message)
@@ -45,7 +44,7 @@ func TestPath(t *testing.T) {
 	}
 }
 
-func testRoute(rt routeTest) (int, string, bool) {
+func testSingleRoute(rt routeTestCase) (int, string, bool) {
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("succesfully"))
 	}
@@ -69,4 +68,82 @@ func testRoute(rt routeTest) (int, string, bool) {
 	}
 
 	return res.Code, content.String(), true
+}
+
+func TestRouterWithMultiRoutes(t *testing.T) {
+	router := Classic()
+
+	handler := func(key string) func(w http.ResponseWriter, r *http.Request) {
+		return func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte(key))
+		}
+	}
+
+	paths := map[string]struct {
+		key  string
+		path string
+	}{
+		"/api/user/1/comment/1": {
+			key:  "0",
+			path: "/api/user/1/comment/1",
+		},
+		"/api/echo": {
+			key:  "1",
+			path: "/api/echo",
+		},
+		"/api/user/1/comments/2": {
+			key:  "2",
+			path: "/api/user/1/comments/2",
+		},
+		"/api/user/donutloop": {
+			key:  "3",
+			path: "/api/user/donutloop",
+		},
+		"/api/user/6": {
+			key:  "4",
+			path: "/api/user/6",
+		},
+		"/api/article/golang": {
+			key:  "6",
+			path: "/api/article/golang",
+		},
+		"/api/article/7": {
+			key:  "7",
+			path: "/api/article/7",
+		},
+		"/api/article/97/comment/9": {
+			key:  "8",
+			path: "/api/article/97/comment/9",
+		},
+	}
+
+	for path, pathInfo := range paths {
+		router.Get(path, handler(pathInfo.key))
+	}
+
+	server := httptest.NewServer(router)
+
+	for rawPath, pathInfo := range paths {
+		url := server.URL + pathInfo.path
+		t.Run(fmt.Sprintf("RawPath: %s, Path: %s Url: %s", rawPath, pathInfo.path, url), func(t *testing.T) {
+			res, err := http.Get(url)
+
+			if err != nil {
+				t.Errorf("Unexpected error (%s)", err.Error())
+			}
+
+			var content bytes.Buffer
+			_, err = io.Copy(&content, res.Body)
+
+			if err != nil {
+				t.Errorf("Unexpected error (%s)", err.Error())
+			}
+
+			defer res.Body.Close()
+
+			if content.String() != pathInfo.key {
+				t.Errorf("Path %s: Unexpected path key (Expected path key: %s, Actucal path key %s)", rawPath, pathInfo.key, content.String())
+			}
+		})
+	}
 }
