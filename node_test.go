b7c27aa1a821073977e228b7dc3b040caf87e3cb
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2017-01-01 11:19:15 +0100

    Added test cases for replace edge

--- node_test.go
+++ node_test.go
@@ -1,8 +1,72 @@
 package tmux
 
-import (
-	"testing"
-)
+import "testing"
+
+func TestReplaceEdge(t *testing.T) {
+
+	parentNode := &Node{
+		edges: [edgeTypes]Edges{},
+	}
+
+	edges := []*Edge{
+		&Edge{
+			label: "a"[0],
+			typ:   paramNode,
+			node: &Node{
+				prefix: "artcile/:number",
+			},
+		},
+		&Edge{
+			label: ":"[0],
+			typ:   paramNode,
+			node: &Node{
+				prefix: ":number",
+			},
+		},
+		&Edge{
+			label: "u"[0],
+			typ:   staticNode,
+			node: &Node{
+				prefix: "user",
+			},
+		},
+	}
+
+	for _, edge := range edges {
+		parentNode.edges[edge.typ] = append(parentNode.edges[edge.typ], edge)
+	}
+
+	newEdge := &Edge{
+		label: "a"[0],
+		typ:   staticNode,
+		node: &Node{
+			prefix: "article/",
+		},
+	}
+
+	if err := parentNode.ReplaceEdge(newEdge); err != nil {
+		t.Errorf("Unexpected error while replaceing (%s)", err.Error())
+	}
+}
+
+func TestReplaceEdgeFail(t *testing.T) {
+
+	parentNode := &Node{
+		edges: [edgeTypes]Edges{},
+	}
+
+	newEdge := &Edge{
+		label: "a"[0],
+		typ:   staticNode,
+		node: &Node{
+			prefix: "article/",
+		},
+	}
+
+	if err := parentNode.ReplaceEdge(newEdge); err == nil {
+		t.Errorf("Unexpected success while replaceing")
+	}
+}
 
 func TestNodeAddType(t *testing.T) {
 
