00695b9240e6f77387d936034a20692c75ecc964
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2017-04-11 22:41:18 +0200

    - Updated: Added regex matcher and extract parameters feature

--- tree_test.go
+++ tree_test.go
@@ -1,46 +1,49 @@
 package tmux
 
 import (
-
 	"testing"
 )
 
 func TestTree_Insert(t *testing.T) {
 
-	testCases := []struct{
+	testCases := []struct {
 		rawPath string
-		path string
+		path    string
 	}{
 		{
 			rawPath: "/home/user/comment/sub",
-			path: "/home/user/comment/sub",
+			path:    "/home/user/comment/sub",
 		},
 		{
 			rawPath: "/home/user/comment",
-			path: "/home/user/comment",
+			path:    "/home/user/comment",
 		},
 
 		{
 			rawPath: "/home/user/article/comment",
-			path: "/home/user/article/comment",
+			path:    "/home/user/article/comment",
 		},
 		{
 			rawPath: "/home/user/article/comment/:string",
-			path: "/home/user/article/comment/test",
+			path:    "/home/user/article/comment/test",
 		},
 		{
 			rawPath: "/:string/:string/:string/:string/:string",
-			path: "/dummy/dummy/dummy/dummy/dummy",
+			path:    "/dummy/dummy/dummy/dummy/dummy",
 		},
 		{
 			rawPath: "/:number/:number/:number/:number/:number",
-			path: "/1/1/1/1/1",
+			path:    "/1/1/1/1/1",
+		},
+		{
+			rawPath: "/#([0-9]{3,})/:number/:number/:number/:number",
+			path:    "/140/1/1/1/1",
 		},
 	}
 
 	tree := NewTree(NewNode)()
 
-	for _, testCase := range testCases{
+	for _, testCase := range testCases {
 		route := NewRoute()
 		route.SetPattern(testCase.rawPath)
 		route = tree.Insert(route)
@@ -55,8 +58,8 @@ func TestTree_Insert(t *testing.T) {
 		}
 	}
 
-	for _, testCase := range testCases{
-		route := tree.Find(tree.GetRoot(), MethodGet, testCase.path)
+	for _, testCase := range testCases {
+		route, _ := tree.Find(tree.GetRoot(), MethodGet, testCase.path)
 
 		if route == nil {
 			t.Errorf("Unexpected nil route (Expected: %s)", testCase.path)
