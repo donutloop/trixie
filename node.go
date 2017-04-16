5dd1d1ade6352865f32c164ba44ba02e95bff23c
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2017-04-16 09:49:45 +0200

    - Updated: doc - Updated: Comments of functions - Cleanup: Removed unused code

--- node.go
+++ node.go
@@ -2,6 +2,7 @@ package trixie
 
 type nodeType int
 
+// All kind of Nodes
 const (
 	staticNode nodeType = iota
 	paramNode
@@ -9,29 +10,29 @@ const (
 	nodeTypes
 )
 
+// NewNode creates a Node instance and setup place for sub nodes
 func NewNode() *Node {
 	nodes := [nodeTypes][]*Node{}
 	nodes[staticNode] = make([]*Node, 0, 0)
 	nodes[paramNode] = make([]*Node, 0, 0)
 	nodes[regexNode] = make([]*Node, 0, 0)
-
 	return &Node{
 		nodes: nodes,
 	}
 }
 
 type Node struct {
+	// is current node the root node of a tree
 	root bool
 
-	// leaf is used to store possible leaf
+	// leaf (Route instance) is used to store possible leaf
 	leaf RouteInterface
 
-	// Edges should be stored in-order for iteration.
+	// Node should be stored in-order for iteration.
 	// We avoid a fully materialized slice to save memory,
 	// since in most cases we expect to be sparse
 	nodes [nodeTypes][]*Node
 
+	// Segment of an path
 	seg string
-
-	param map[string]string
 }
